379f42ffcf2d6a262b766408d3a6140b89ea8fb1
pedro <pedrofrancach@gmail.com>
2023-05-03 12:45:45 -0300

    commit

--- jump/jump.rb
+++ jump/jump.rb
@@ -1,20 +1,34 @@
 #Jump Search
 # Esse conceito é baseado em um elemento previamente ordenado, essa funcionalidade
 # de busca não funcionará em um elemento desordenado  
-#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
+#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
 
-def Jump(arraye)
+def Jump(arraye, valor)
 
+arrayi = []
+cont1 = 0.to_i
+cont2 = 0.to_i
+tamanho = arraye.length
 
+while cont1 <= tamanho do
+    
+    if arraye[cont1] > valor
+    
+            
+    
+    end
 
+end
 
 
 end
 
-#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
+#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
 
 puts "O programa foi iniciado!"
 
 lista = [0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610]
 
-jump(lista)
+puts "Digite o valor a ser encontrado"
+
+jump(lista, valor)
