3256aa92ce1bbeec6f0583662f502783719372aa
pedro <pedrofrancach@gmail.com>
2023-05-04 15:03:19 -0300

    commit

--- Find/find.rb
+++ Find/find.rb
@@ -3,12 +3,40 @@
 #
 def find_miss(arraye)
 
-cont1 =0.to_i
+cont1 = 0.to_i
+cont2 = 0.to_i
+vef1 = 0.to_i
 tamanho = arraye.length
+arrayi = arraye #ordenado
+arrayp = []
+calc = 0.to_i
+
 
 while cont1 < tamanho do
 
-    
+    if vef1 == 0
+
+        vef1 = arrayi[0]
+
+    end
+
+    if vef1 != arrayi[cont1]
+
+        calc = arrayi[cont1] - vef1 - 1
+        
+        until cont2 == calc do
+
+            arrayp << vef1 + 1
+
+            cont2 = cont2 + 1
+
+        end
+        
+        cont2 = 0.to_i
+
+    end
+
+    vef1 = vef1 + 1
 
 end
 
