9093b9c7d66f1e68476f0c21d48fc200ec0b336c
pedro <pedrofrancach@gmail.com>
2023-05-04 14:39:27 -0300

    commit

--- Find/find.rb
+++ Find/find.rb
@@ -1,1 +1,23 @@
 #Find the missing number
+#
+#
+def find_miss(arraye)
+
+cont1 =0.to_i
+tamanho = arraye.length
+
+while cont1 < tamanho do
+
+    
+
+end
+
+
+
+end
+#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*###*#*#*#*#*#*#*#*#*#*#
+
+
+lista = [1,2,4,5,6,8]
+
+find_miss(lista)
