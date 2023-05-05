d3d51c3f9d4b958977b805120331419b97fcffe8
pedro <pedrofrancach@gmail.com>
2023-05-05 17:40:49 -0300

    commit

--- Find/find.rb
+++ Find/find.rb
@@ -6,7 +6,7 @@ def find_miss(arraye)
 cont1 = 0.to_i
 cont2 = 0.to_i
 vef1 = 0.to_i
-tamanho = arraye.length
+tamanho = arraye.length + 1
 arrayi = arraye.sort  
 arrayp = []
 calc = 0.to_i
@@ -31,30 +31,34 @@ while cont1 < tamanho do
         calc = arrayi[cont1] - vef1 
         puts "entrou vef1 é #{vef1} e arrayi[cont1] é #{arrayi[cont1]} e calc é #{calc}" 
         vef2 = vef1
-        until cont2 != calc do
-           #puts "entrou1"
+        puts "cont2 é #{cont2}"
+     
+        while cont2 < calc do
+           puts "cont2 é #{cont2} e calc é #{calc}"
             arrayp << vef2 
  
             cont2 = cont2 + 1
             vef2 = vef2 + 1
+          #  vef1 = vef1 + 1
         end
 
         cont2 = 0.to_i
-
+        #vef1 = vef1 + 1
     end
 
+   
     vef1 = vef1 + 1
     cont1 = cont1 + 1
 
 end
 
 puts arrayp
-
+puts "saiu"
 end
 #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*###*#*#*#*#*#*#*#*#*#*#
 
 puts "o programa iniciou"
-lista = [1,4,2,5,8,6]
+lista = [1,4,5]
 
 find_miss(lista)
  
