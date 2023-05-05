ecd7d43bcd79d60d5629fdaa1a161a39202f0401
pedro <pedrofrancach@gmail.com>
2023-05-05 16:48:35 -0300

    commit

--- Find/find.rb
+++ Find/find.rb
@@ -7,13 +7,17 @@ cont1 = 0.to_i
 cont2 = 0.to_i
 vef1 = 0.to_i
 tamanho = arraye.length
-arrayi = arraye #ordenado
+arrayi = arraye.sort  
 arrayp = []
 calc = 0.to_i
 
+puts arrayi
+puts "caseta"
+puts arrayp
+puts "fim caseta"
 
 while cont1 < tamanho do
-
+#puts "entrou0"
     if vef1 == 0
 
         vef1 = arrayi[0]
@@ -24,28 +28,30 @@ while cont1 < tamanho do
 
         calc = arrayi[cont1] - vef1 - 1
         
-        until cont2 == calc do
-
+        until cont2 != calc do
+           #puts "entrou1"
             arrayp << vef1 + 1
 
-            cont2 = cont2 + 1
+            cont2 += 1
 
         end
-        
+
         cont2 = 0.to_i
 
     end
 
     vef1 = vef1 + 1
+    cont1 = cont1 + 1
 
 end
 
-
+#puts arrayp
 
 end
 #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*###*#*#*#*#*#*#*#*#*#*#
 
-
-lista = [1,2,4,5,6,8]
+puts "o programa iniciou"
+lista = [1,4,2,5,8,6]
 
 find_miss(lista)
+ 
