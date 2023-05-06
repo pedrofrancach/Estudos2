5f07ebcf3d15ed5514f8e4b139b9e8a34f38993e
pedro <pedrofrancach@gmail.com>
2023-05-06 12:37:52 -0300

    commit

--- Find/find.rb
+++ Find/find.rb
@@ -13,55 +13,50 @@ calc = 0.to_i
 vef2 = 0.to_i
 numero = 1.to_i
 
-#puts arrayi
-#puts "caseta"
-#puts arrayp
-#puts "fim caseta"
+
+
 
 while cont1 < tamanho do
-#puts "entrou0"
+
 
     if vef1 == 0
-        #puts "brincadeira vef1 #{vef1}"
+       
         vef1 = arrayi[0]
-        #puts "Desbrincadeira vef1 #{vef1}" 
+    
     end
-    puts "numero é #{numero}"
-    puts "entrou vef1 é #{vef1} e arrayi[cont1] é #{arrayi[cont1]} e calc é #{calc}" 
+   
     if vef1 != arrayi[cont1]
        
         calc = arrayi[cont1].to_i - vef1.to_i 
-        #puts "entrou vef1 é #{vef1} e arrayi[cont1] é #{arrayi[cont1]} e calc é #{calc}" 
         vef2 = vef1
-        puts "cont2 é #{cont2}"
-     
+   
         while cont2 < calc do
-           puts "cont2 é #{cont2} e calc é #{calc} e vef2 é #{vef2}"
-           arrayp << vef2 
- 
+        
+            arrayp << vef2 
             cont2 = cont2 + 1
             vef2 = vef2 + 1
-          #  vef1 = vef1 + 1
+         
         end
 
         cont2 = 0.to_i
-        vef1 = calc + 1
+        vef1 = calc + vef2
         calc = 0.to_i
-         
+         puts "essa caseta é #{vef1}"
+    
     end
-
    
     vef1 = vef1 + 1
     cont1 = cont1 + 1
     numero =  numero + 1
+
 end
 
 puts arrayp
-puts "saiu"
+
 end
 #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*###*#*#*#*#*#*#*#*#*#*#
 
-puts "o programa iniciou"
+
 lista = [1,4,5]
 
 find_miss(lista)
