aeda6a9c48ed70444920144f03d893eb08416083
pedro <pedrofrancach@gmail.com>
2023-05-17 22:01:16 -0300

    commit

---
+++ selec/selec.rb
@@ -0,0 +1,25 @@
+#Selection Sort
+#
+#
+def selec(arraya, arrayb)
+
+tamanho = arraya.length
+cont = 0.to_i
+arrayi = arraya
+
+while cont < tamanho do
+
+
+    
+end
+
+
+end
+
+#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
+
+listaa = [8,7,]
+listab = []
+
+
+selec(listaa, listab)
