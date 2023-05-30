0903995c96df34aa5fa9bd7c1c61de025ba8cc92
pedro <pedrofrancach@gmail.com>
2023-05-29 22:33:41 -0300

    commit

--- Ssort/sort.rb
+++ Ssort/sort.rb
@@ -2,8 +2,24 @@
 
 def selec (arraye)
 
+tamanho = arraye.length
+cont = 0.to_i
+reserv = 0.to_i
+
+while cont < tamanho do
+
+    if arraye[cont] > arraye[cont + 1]
+    
+       reserv = arraye[cont + 1]
+       arraye[cont + 1] = arraye[cont]
+       arraye[cont] = reserv 
+    end 
+
+
+reserv = 0.to_i
 end
 
+end
 
 #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
 
